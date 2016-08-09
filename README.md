9f89221fa786068f714c61c08f7db9e4a1cb04f5
kwall2004 <kwall2004@gmail.com>
2016-08-09 16:03:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-demo-api
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CounterController.js
@@ -0,0 +1,11 @@
+/**
+ * CountersController
+ *
+ * @description :: Server-side logic for managing counters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
