b3e4c1fe6fc9be8116a4ba58513d0f07e12ae000
kwall2004 <kwall2004@gmail.com>
2016-09-12 16:42:08 -0400

    add vehicles

--- api/models/Vehicle.js
+++ api/models/Vehicle.js
@@ -6,9 +6,23 @@
  */
 
 module.exports = {
-
-  attributes: {
-
-  }
+    tableName: 'Vhcl',
+    autoPK: false,
+    autoCreatedAt: false,
+    autoUpdatedAt: false,
+    attributes: {
+        VhclID: { type: 'integer' },
+        BsnsInfoID: { type: 'integer' },
+        VIN: { type: 'string' },
+        Make: { type: 'string' },
+        Modl: { type: 'string' },
+        ModlYear: { type: 'string' },
+        Img: { type: 'string' },
+        Alas: { type: 'string' },
+        Regs: { type: 'string' },
+        VhclStts: { type: 'boolean' },
+        InitOdo: { type: 'float' },
+        SpedThrs: { type: 'integer' },
+        RPMThrs: { type: 'integer' },
+    }
 };
-
